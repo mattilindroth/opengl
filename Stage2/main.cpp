b1f25ede64483dfc8d84ea50995660bdc381528d
Matti Lindroth <matti.lindroth@gmail.com>
2020-11-11 22:45:42 +0200

    Stuff

--- Stage2/main.cpp
+++ Stage2/main.cpp
@@ -32,17 +32,28 @@ int main(void)
         return 0;
     }
 
+    float positions[6] = {
+       -0.5f, -0.5f,
+        0.0f,  0.5f,
+        0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 3, null);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
