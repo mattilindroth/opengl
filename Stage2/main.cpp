3783206bc57f293f2f34f684adfd4907be60027a
Matti Lindroth <matti.lindroth@gmail.com>
2020-11-19 16:49:45 +0200

    Added simple vertex and fragment shaders. Tried to draw red triangle. Got errors

--- Stage2/main.cpp
+++ Stage2/main.cpp
@@ -4,8 +4,49 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <string>
 
-using namespace std;
+static unsigned int CompileShader(unsigned int  type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char *src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if(result == GL_FALSE) 
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char *message = (char*)malloc(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile header " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        free(message);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 
 int main(void)
@@ -28,7 +69,7 @@ int main(void)
     glfwMakeContextCurrent(window);
     GLenum err = glewInit();
     if(err != GLEW_OK) {
-        cout << "Error while initializing Glew! glewInit returned " << err << endl;
+        std::cout << "Error while initializing Glew! glewInit returned " << err << std::endl;
         return 0;
     }
 
@@ -46,6 +87,24 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    std::string vertexShader = "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 position\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    std::string fragmentShader = "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -61,7 +120,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    glDeleteShader(shader);
+    
     glfwTerminate();
     return 0;
 }
